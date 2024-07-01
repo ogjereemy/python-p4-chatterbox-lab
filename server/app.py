f9c9b53ff608699580bb601214edff63ff65d6ad
ogola <ogolajeremy601@gmail.com>
2024-07-01 20:13:49 +0300

    adds chatterbox lab

--- server/app.py
+++ server/app.py
@@ -16,11 +16,59 @@ db.init_app(app)
 
 @app.route('/messages')
 def messages():
-    return ''
+    messages = [message.to_dict() for message in Message.query.all()]
 
-@app.route('/messages/<int:id>')
+    response = make_response(messages, 200)
+
+    return response
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    body = request.json.get('body')
+    username = request.json.get('username')
+
+    if not body or not username:
+        return make_response({'error': 'Missing body or username'}, 400)
+
+    message = Message(body=body, username=username)
+    db.session.add(message)
+    db.session.commit()
+
+    return make_response(message.to_dict(), 201)
+    
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
 def messages_by_id(id):
-    return ''
+    message = db.session.get(Message, id)
+
+    if not message:
+        return make_response({'error': 'Message not found'}, 404)
+
+    body = request.json.get('body')
+    username = request.json.get('username')
+
+    if body:
+        message.body = body
+
+    if username:
+        message.username = username
+
+    db.session.commit()
+
+    return make_response(message.to_dict(), 200)
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+
+    if not message:
+        return make_response({'error': 'Message not found'}, 404)
+
+    db.session.delete(message)
+    db.session.commit()
+
+    return make_response('', 204)
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
